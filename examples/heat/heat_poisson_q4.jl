2106b7b487850fd46c9a40c5fa9b0b9665dfceb2
Petr Krysl <pkrysl@ucsd.edu>
2020-05-25 08:27:12 -0700

    cleaned up

--- examples/heat/heat_poisson_q4.jl
+++ examples/heat/heat_poisson_q4.jl
@@ -31,9 +31,6 @@ end
 function integrateK!(ass, geom, elit, qpit)
     vmdim = Val(manifdim(refshape(elit.fesp.fe)))
     nedof = ndofsperelem(elit)
-    is = fill(0, nedof*nedof)
-    js = fill(0, nedof*nedof)
-    vs = fill(0.0, nedof*nedof)
     for el in elit
         for qp in qpit
             gradNparams = bfungradpar(qp)
@@ -41,44 +38,19 @@ function integrateK!(ass, geom, elit, qpit)
             J = Jacobian(vmdim, Jac)
             JxW = J * weight(qp)
             invJac = inv(Jac)
-            k = 1
             for j in 1:nedof
                 gradNj = gradNparams[j] * invJac
                 for i in 1:nedof
                     gradNi = gradNparams[i] * invJac
                     v = dot(gradNi, gradNj) * JxW 
                     asstolma!(el, i, j, v)
-                    # el._lma.M[i, j] += v
-                    # is[k] = elemdofs(el)[i]
-                    # js[k] = elemdofs(el)[j]
-                    # vs[k] = v
-                    k = k + 1
                 end
             end
         end
         assemble!(ass, lma(el)...)
-        # assemble!(ass, el._lma.row, el._lma.col, vec(el._lma.M))
-        # assemble!(ass, is, js, vs)
     end
     return ass
 end
-
-# function f(qpit) 
-#     # @show typeof(qpit)
-#     gradNparams = bfungradpar(qpit)
-#     # gradNparams = weight(qpit)
-# end
-
-# function f(d, j)
-#     # gradNparams = bfungradpar(qpit)
-#     gradNparams = d[j]
-# end
-# function f() 
-#     # @show typeof(qpit)
-#     qpit = QPIterator(fesp.fe, (kind = :Gauss, order = 2))
-#     gradNparams = bfungradpar(qpit)
-#     # gradNparams = weight(qpit, 1)
-# end
 
 function assembleK(fesp)
     elit = FEIterator(fesp)
@@ -86,8 +58,6 @@ function assembleK(fesp)
     geom = geometry(elit)
     ass = SysmatAssemblerSparse(0.0)
     start!(ass, ndofs(fesp), ndofs(fesp))
-    # @code_warntype f(qpit)
-    # @code_warntype f(qpit._bfungraddata, qpit._pt[])
     @time integrateK!(ass, geom, elit, qpit)
     return finish!(ass)
 end
