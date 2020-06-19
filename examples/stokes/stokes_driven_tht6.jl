4a5a94716a5bd72393353f3b7c2f96468f80ecdb
Petr Krysl <pkrysl@ucsd.edu>
2020-06-19 09:45:10 -0700

    Stokes correct

--- examples/stokes/stokes_driven_tht6.jl
+++ examples/stokes/stokes_driven_tht6.jl
@@ -8,21 +8,22 @@ using MeshSteward: Mesh, insert!, baseincrel, boundary
 using MeshSteward: vselect, geometry, summary
 using MeshSteward: vtkwrite
 using Elfel.RefShapes: manifdim, manifdimv
-using Elfel.FElements: FEH1_T6, refshape, Jacobian
+using Elfel.FElements: FEH1_T6, FEH1_T3, refshape, Jacobian
 using Elfel.FESpaces: FESpace, ndofs, setebc!, nunknowns, doftype
 using Elfel.FESpaces: numberfreedofs!, numberdatadofs!
 using Elfel.FESpaces: scattersysvec!, makeattribute, gathersysvec!, edofcompnt
+using Elfel.FESpaces: highestfreedofnum, highestdatadofnum
 using Elfel.FEIterators: FEIterator, ndofsperel, elnodes, eldofs
 using Elfel.FEIterators: jacjac
-using Elfel.AggregateFEIterators: AggregateFEIterator, iterator
 using Elfel.QPIterators: QPIterator, bfun, bfungrad, weight
 using Elfel.Assemblers: SysmatAssemblerSparse, start!, finish!, assemble!
 using Elfel.Assemblers: SysvecAssembler
 using Elfel.LocalAssemblers: LocalMatrixAssembler, LocalVectorAssembler, init!
+using UnicodePlots
 
 mu = 0.25 # dynamic viscosity
 A = 1.0 # length of the side of the square
-N = 4;# number of subdivisions along the sides of the square domain
+N = 10;# number of subdivisions along the sides of the square domain
 
 function genmesh()
     # Taylor-Hood pair of meshes is needed
@@ -32,39 +33,80 @@ function genmesh()
     # This mesh will be used for the pressures
     pmesh = Mesh()
     insert!(pmesh, T6toT3(baseincrel(vmesh, "velocity")), "pressure")
-  return vmesh, pmesh
+    return vmesh, pmesh
 end
 
 function assembleK(uxfesp, uyfesp, pfesp, tndof, mu)
-    function integrateK!(ass, geom, elit, qpit, mu)
+    function integrateK!(ass, elit, qpit, mu)
+        uxnedof, uynedof, pnedof = ndofsperel.(elit.is)
+        kuxux = LocalMatrixAssembler(uxnedof, uxnedof, 0.0)
+        kuyuy = LocalMatrixAssembler(uynedof, uynedof, 0.0)
+        kuxuy = LocalMatrixAssembler(uxnedof, uynedof, 0.0)
+        kuxp = LocalMatrixAssembler(uxnedof, pnedof, 0.0)
+        kuyp = LocalMatrixAssembler(uynedof, pnedof, 0.0)
         for el in elit
-            uxel = iterator(el, 1)
-            uyel = iterator(el, 2)
-            pel = iterator(el, 3)
+            uxel = el[1]
+            uyel = el[2]
+            pel = el[3]
+            init!(kuxux, eldofs(uxel), eldofs(uxel))
+            init!(kuyuy, eldofs(uyel), eldofs(uyel))
+            init!(kuxuy, eldofs(uxel), eldofs(uyel))
+            init!(kuxp, eldofs(uxel), eldofs(pel))
+            init!(kuyp, eldofs(uyel), eldofs(pel))
             for qp in qpit
-                Jac, J = jacjac(el, qp)
-                gradN = bfungrad(qp, Jac)
-                JxW = J * weight(qp)
-                for j in 1:nedof
-                    DBj = D * B(gradN[j], c[j])
-                    for i in 1:nedof
-                        Bi = B(gradN[i], c[i])
-                        v = dot(DBj, Bi) * JxW
-                        asstolma!(el, i, j, v)
+                uxqp = qp[1]
+                uyqp = qp[2]
+                pqp = qp[3]
+                Jac, J = jacjac(pel, pqp)
+                JxW = J * weight(pqp)
+                gradNp = bfungrad(pqp, Jac)
+                gradNux = bfungrad(uxqp, Jac)
+                gradNuy = bfungrad(uyqp, Jac)
+                Np = bfun(pqp)
+                for j in 1:uxnedof
+                    for i in 1:uxnedof
+                        kuxux[i, j] += (mu * JxW) * (2 * gradNux[i][1] * gradNux[j][1] + gradNux[i][2] * gradNux[j][2])
+                    end
+                end
+                for j in 1:uynedof
+                    for i in 1:uynedof
+                        kuyuy[i, j] += (mu * JxW) * (gradNuy[i][1] * gradNuy[j][1] + 2 * gradNuy[i][2] * gradNuy[j][2])
+                    end
+                end
+                for j in 1:uynedof
+                    for i in 1:uxnedof
+                        kuxuy[i, j] += (mu * JxW) * (gradNux[i][1] * gradNuy[j][2])
+                    end
+                end
+                for j in 1:pnedof
+                    for i in 1:uxnedof
+                        kuxp[i, j] += (-JxW) * (gradNux[i][1] * Np[j])
+                    end
+                end
+                for j in 1:pnedof
+                    for i in 1:uynedof
+                        kuyp[i, j] += (-JxW) * (gradNuy[i][2] * Np[j])
                     end
                 end
             end
-            assemble!(ass, lma(el)...)
+            assemble!(ass, kuxux)
+            assemble!(ass, kuxuy)
+            assemble!(ass, transpose(kuxuy))
+            assemble!(ass, kuyuy)
+            assemble!(ass, kuxp)
+            assemble!(ass, transpose(kuxp))
+            assemble!(ass, kuyp)
+            assemble!(ass, transpose(kuyp))
         end
         return ass
     end
 
-    elit = AggregateFEIterator([FEIterator(uxfesp), FEIterator(uyfesp), FEIterator(pfesp)])
-    qpit = QPIterator(fesp, (kind = :default, npts = 3,))
-    geom = geometry(fesp.mesh)
+    elit = zip(FEIterator(uxfesp), FEIterator(uyfesp), FEIterator(pfesp))
+    qargs = (kind = :default, npts = 3,)
+    qpit = zip(QPIterator(uxfesp, qargs), QPIterator(uyfesp, qargs), QPIterator(pfesp, qargs))
     ass = SysmatAssemblerSparse(0.0)
     start!(ass, tndof, tndof)
-    @time integrateK!(ass, geom, elit, qpit, D)
+    @time integrateK!(ass, elit, qpit, mu)
     return finish!(ass)
 end
 
@@ -98,30 +140,42 @@ function run()
         setebc!(uyfesp, 0, i, 1, 0.0)
     end
     # Pressure space
-    pfesp = FESpace(Float64, pmesh, FEH1_T6(), 1)
+    pfesp = FESpace(Float64, pmesh, FEH1_T3(), 1)
+    setebc!(pfesp, 0, 1, 1, 0.0)
     # Number the degrees of freedom
-    numberfreedofs!(uxfesp)
-    numberdatadofs!(uxfesp)
-    numberfreedofs!(uyfesp, nunknowns(uxfesp)+1)
-    numberdatadofs!(uyfesp)
-    numberfreedofs!(pfesp, nunknowns(uyfesp)+1)
-    numberdatadofs!(pfesp)
+    numberfreedofs!(uxfesp, 1)
+    numberfreedofs!(uyfesp, highestfreedofnum(uxfesp)+1)
+    numberfreedofs!(pfesp, highestfreedofnum(uyfesp)+1)
+    numberdatadofs!(uxfesp, highestfreedofnum(pfesp)+1)
+    numberdatadofs!(uyfesp, highestdatadofnum(uxfesp)+1)
+    numberdatadofs!(pfesp, highestdatadofnum(uyfesp)+1)
+    @show highestfreedofnum(uxfesp), highestdatadofnum(uxfesp)
+    @show highestfreedofnum(uyfesp), highestdatadofnum(uyfesp)
+    @show highestfreedofnum(pfesp), highestdatadofnum(pfesp)
+
     @show ndofs(uxfesp)
     @show ndofs(uyfesp)
     @show ndofs(pfesp)
     @show nunknowns(uxfesp)
     @show nunknowns(uyfesp)
     @show nunknowns(pfesp)
-    tndof = ndofs(uxfesp) + ndofs(uyfesp) + ndofs(pfesp)
-    tnunk = nunknowns(uxfesp) + nunknowns(uyfesp) + nunknowns(pfesp)
+    @show tndof = ndofs(uxfesp) + ndofs(uyfesp) + ndofs(pfesp)
+    @show tnunk = nunknowns(uxfesp) + nunknowns(uyfesp) + nunknowns(pfesp)
     K = assembleK(uxfesp, uyfesp, pfesp, tndof, mu)
-    U = fill(0.0, ndofs(fesp))
-    gathersysvec!(U, fesp)
+    p = spy(K, canvas = DotCanvas)
+    display(p)
+    U = fill(0.0, tndof)
+    gathersysvec!(U, uxfesp)
+    gathersysvec!(U, uyfesp)
+    gathersysvec!(U, pfesp)
     F = fill(0.0, tndof)
     solve!(U, K, F, tnunk)
-    scattersysvec!(fesp, U)
-    makeattribute(fesp, "U", 1:2)
-    vtkwrite("stokes_driven_tht6-U", baseincrel(mesh), [(name = "U", allxyz = true)])
+    scattersysvec!(uxfesp, U)
+    scattersysvec!(uyfesp, U)
+    scattersysvec!(pfesp, U)
+    makeattribute(pfesp, "p", 1)
+    vtkwrite("stokes_driven_tht6-p", baseincrel(pmesh), [(name = "p",)])
+    # vtkwrite("stokes_driven_tht6-p", baseincrel(pmesh), [(name = "p", allxyz = true)])
 end
 
 end
