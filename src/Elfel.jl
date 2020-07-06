4804b2bd10589c9d6737144902a8ff8669fc3f7d
Petr Krysl <pkrysl@ucsd.edu>
2020-07-06 11:23:23 -0700

    explain exports

--- src/Elfel.jl
+++ src/Elfel.jl
@@ -7,11 +7,18 @@ include("LocalAssemblers.jl")
 include("Assemblers.jl")
 include("RefShapes.jl")
 include("FElements.jl")
-# include("FEFields.jl")
 include("FESpaces.jl")
 include("QPIterators.jl")
 include("FEIterators.jl")
 
+# We can either use/import individual functions from Elfel like so:
+# ```
+# using Elfel.FESpaces: numberfreedofs!, numberdatadofs!, numberdofs!
+# ```
+# or we can bring into our context all exported symbols as
+# ```
+# using Elfel.Exports
+# ```
 include("Exports.jl")
 
 end # module
